0e221a0cce1abfeee032da8faf702fb81dbbe277
Stefan <stefan3012@hotmail.de>
2017-03-08 00:26:48 +0100

    converted script.js from jquery to plain javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,66 +1,107 @@
-$(function() {
-	$(".btn-add2fav").click(function(event) {
-        event.preventDefault();
+const $ = document.querySelector.bind(document);
 
-        const self = event.target;
-        const movieId = self.dataset.id;
-        const btn = $(this);
-        const favcounter = self.parentElement.childNodes[3].childNodes[1];
+function ready(fn) {
+    if (document.readyState != "loading") fn();
+    else document.addEventListener("DOMContentLoaded", fn);
+}
 
-        self.setAttribute("disabled", true);
-        self.innerText = "Fav'd";
-        $.post("/movies/" + movieId + "/fav", (data) => {
-            favcounter.innerText = data.favs;
-            setTimeout(() => { self.removeAttribute("disabled"); self.innerText = "Add to favorites"; }, 500);
-        });
-    });
-    $(".btn-fav").click(function(event) {
-        event.preventDefault();
+function makePOSTRequest(url, callback) {
+    const http = new XMLHttpRequest();
+    http.open("POST", url, true);
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200)
+            callback(JSON.parse(http.responseText));
+    }
+    http.send(null);
+}
 
-        const self = event.target;
-        const movieId = self.dataset.id;
-        const favcounter = self.parentElement.parentElement.childNodes[3].childNodes[1];
-        const favCounterCard = document.querySelector(".card-land [data-id='" + movieId + "']").parentElement.childNodes[3].childNodes[1];
+ready(() => {
+    document.querySelectorAll(".btn-add2fav").forEach((btn) => {
+        btn.addEventListener("click", function (event) {
+            event.preventDefault();
 
-        self.setAttribute("disabled", true);
-        self.innerText = "Fav'd";
-        $.post("/movies/" + movieId + "/fav", (data) => {
-            favcounter.innerText = data.favs;
-            favCounterCard.innerText = data.favs;
-            setTimeout(() => { self.removeAttribute("disabled"); self.innerText = "Add to favorites"; }, 500);
-        });
-    });
+            const self = event.target;
+            const movieId = self.dataset.id;
+            const favcounter = self.parentElement.childNodes[3].childNodes[1];
 
-    $(".btn-expand").click(function(event) {
-        $('.overlay').addClass('is-open');
-        const movieId = $(this).data("id");
-
-        const modal = $(".modal-movie");
-        const poster = modal.children("div.img-left").children("img.poster");
-        const details = modal.children(".details-block");
-        const favs = details.children(".favs");
-        const title = details.children(".title");
-        const year = details.children("small.year");
-        const genres = details.children(".genres");
-        const cast = details.children(".cast");
-        const plot = details.children(".plot");
-
-        $.post("/movies/" + movieId, (movie) => {
-            modal.css("background-image", "linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url('https://image.tmdb.org/t/p/w1280"+ movie.backdrop_path +"')");
-            poster.attr("src", "https://image.tmdb.org/t/p/w500" + movie.poster_path);
-            poster.attr("alt", movie.title + " poster");
-            modal.children("div.img-left").children("button.btn-fav").attr("data-id", movie.id);
-
-            favs.text(movie.favs);
-            title.text(movie.title);
-            year.text(movie.year);
-            genres.text(movie.genres);
-            cast.text(movie.cast);
-            plot.text(movie.plot);
+            self.setAttribute("disabled", true);
+            self.textContent = "Fav'd";
+            makePOSTRequest("/movies/" + movieId + "/fav", function (data) {
+                favcounter.textContent = data.favs;
+                setTimeout(() => {
+                    self.removeAttribute("disabled");
+                    self.textContent = "Add to favorites";
+                }, 500);
+            });
         });
     });
 
-    $(".close-btn").click(function(event) {
-        $('.overlay').removeClass('is-open')
-    });
+    const favbtns = document.querySelectorAll(".btn-fav");
+    if (favbtns.length > 0)
+        favbtns.forEach((btn) => {
+            btn.addEventListener("click", function (event) {
+                event.preventDefault();
+
+                const self = event.target;
+                const movieId = self.dataset.id;
+                const favcounter = self.parentElement.parentElement.childNodes[3].childNodes[1];
+                const favCounterCard = $(".card-land [data-id='" + movieId + "']").parentElement.childNodes[3].childNodes[1];
+
+                self.setAttribute("disabled", true);
+                self.textContent = "Fav'd";
+                makePOSTRequest("/movies/" + movieId + "/fav", (data) => {
+                    favcounter.textContent = data.favs;
+                    favCounterCard.textContent = data.favs;
+                    setTimeout(() => {
+                        self.removeAttribute("disabled");
+                        self.textContent = "Add to favorites";
+                    }, 500);
+                });
+            });
+        });
+
+    const expandbtns = document.querySelectorAll(".btn-expand");
+    if (expandbtns.length > 0) {
+        expandbtns.forEach((btn) => {
+            btn.addEventListener("click", function(event) {
+                // if middle of button is clicked, event.target isn't button but icon
+                let self = event.target;
+                if (self.localName === "i")
+                    self = self.parentElement;
+
+                const movieId = self.dataset.id;
+
+                const modal = $(".modal-movie");
+                const poster = modal.childNodes[1].childNodes[1];
+                const details = modal.childNodes[3];
+                const favs = details.childNodes[1];
+                const title = details.childNodes[3];
+                const year = title.childNodes[0];
+                const genres = details.childNodes[5];
+                const cast = details.childNodes[7];
+                const plot = details.childNodes[9];
+
+                makePOSTRequest("/movies/" + movieId, (movie) => {
+                    modal.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url('https://image.tmdb.org/t/p/w1280" + movie.backdrop_path + "')";
+                    poster.setAttribute("src", "https://image.tmdb.org/t/p/w500" + movie.poster_path);
+                    poster.setAttribute("alt", movie.title + " poster");
+                    // adding data-id to button to be able to favorite
+                    modal.childNodes[1].childNodes[3].setAttribute("data-id", movie.id);
+
+                    favs.textContent = movie.favs;
+                    title.textContent = movie.title;
+                    year.textContent = movie.year;
+                    genres.textContent = movie.genres;
+                    cast.textContent = movie.cast;
+                    plot.textContent = movie.plot;
+    
+                    $(".overlay").classList.add("is-open");
+                });
+            });
+        });
+    }
+    if ($(".close-btn") != null)
+        $(".close-btn").addEventListener("click", function (event) {
+            $(".overlay").classList.remove("is-open");
+        });
 });
