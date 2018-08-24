e9ff116184ed7805f44dc668bebae19ac0e3ca50
Stefan <stefan3012@hotmail.de>
2018-08-24 15:37:27 +0200

    dependencies cleanup & env_mode to production

--- server.js
+++ server.js
@@ -4,27 +4,26 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const errorHandler = require('errorhandler')
-const cookieParser = require('cookie-parser')
 const compression = require('compression')
 const favicon = require('serve-favicon')
-const app = express()
-const port = process.env.PORT || 1337
+const serveStatic = require('serve-static')
 const renderRouter = require('./app/renderRouter')
 const apiRouter = require('./app/apiRouter')
+const app = express()
+const port = process.env.PORT || 1337
 
 app.use(compression())
-app.set('views', __dirname + '/views')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
 
 app.use('/api', apiRouter)
 app.use('/', renderRouter)
-app.use(express.static(__dirname + '/public'))
+app.use(serveStatic(__dirname + '/public', { maxAge: '1d' }))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 if ('development' === app.get('env')) app.use(errorHandler())
 
+app.set('views', __dirname + '/views')
 app.engine('hbs', exphbs({
 	defaultLayout: 'main',
 	extname: '.hbs'
@@ -33,8 +32,10 @@ app.set('view engine', 'hbs')
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }, err => {
 	if (err) throw err
-	const d = new Date()
-	console.log(`[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] mLab connected`)
+	if (app.get('env') === 'development') {
+		const d = new Date()
+		console.log(`[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] mLab connected`)
+	}
 })
 
 app.listen(port, () => {
